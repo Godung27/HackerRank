9d2f8eb986b35c036eea7fd02e26dc5dadd22e0a
Godung27 <parnuvich.l@gmail.com>
2026-06-09 20:53:59 +0700

    Javascript_Time Conversion

---
+++ javascript/Time Conversion.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(s) {
+  // Write your code here
+  const [time, period] = s.split(/(AM|PM)/);
+  let [hh, mm, ss] = time.split(":");
+
+  if (period === "AM") {
+    if (hh === "12") hh = "00";
+  } else {
+    if (hh !== "12") hh = String(Number(hh) + 12);
+  }
+
+  return (`${hh}:${mm}:${ss}`)
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const s = readLine();
+
+  const result = timeConversion(s);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
